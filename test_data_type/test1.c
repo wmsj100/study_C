03f7eb81c01c9daff12db5c3e27cdec4b492c2a8
wmsj100 <wmsj100@hotmail.com>
2019-04-13 16:29:41 +0800

    C puts data_type

---
+++ test_data_type/test1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,c;
+	float m=10.9, n=20.45;
+	char p,q='@';
+
+	printf("a is %d,b is %d,c is %d, float m is %f, float n is %f, str p is %c, ",a,b,c,m,n,q);
+	return 0;
+
+}
---
+++ test_data_type/test_puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	puts(
+			"hello world"
+			"wmsj100"
+			"what is your name!"
+			);
+	puts("aaa" "bbb" "ccc");
+	return 0;
+
+}
